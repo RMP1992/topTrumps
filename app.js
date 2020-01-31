6b0906a5dc05bb61f0ac19b93d3acc69fd766ecb
Code Nation <codenation@Codes-MacBook-Pro.local>
2020-01-31 18:07:01 +0000

    compares the value

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 let randomCardArr = [];
-
+let player1;   
+let player2;
 
 
 class Card  {
-    constructor(name, attack){ 
+    constructor(name, attack, defence, health, evil, rank){ 
         this._name = name;
         this._attack = attack;
+        this._defence = defence;
+        this._health = health;
+        this._evil = evil;
+        this._rank = rank;
+        
     }
     get name(){
         return this._name
@@ -13,39 +19,51 @@ class Card  {
     get attack(){
         return this._attack
     }
+    get defence(){
+        return this._defence
+    }
+    get health(){
+        return this._health
+    }
+    get evil(){
+        return this._evil
+    }
+    get rank(){
+        return this._rank
+    }
     
 }
 const deck = [
-    new Card("Saberleomon", 550),
-    new Card("Wargreymon", 540),
-    new Card("Magnadramon", 520),
-    new Card("Devimon", 350),
-    new Card("Lilymon", 510),
-    new Card("Angemon", 560),
-    new Card("Angewomon", 470),
-    new Card("Rosemon", 570),
-    new Card("Seraphimon", 540),
-    new Card("Ofanimon", 540),
-    new Card("Gatomon", 360),
-    new Card("Nefertimon", 470),
-    new Card("Pagasusmon", 420),
-    new Card("Kyubimon", 450),
-    new Card("Sakuyamon", 590),
-    new Card("Myotismon", 510),
-    new Card("Piedmon", 530),
-    new Card("Puppetmon", 500),
-    new Card("Ladydevimon", 520),
-    new Card("Weregarurumon", 530),
-    new Card("Lynxmon", 480),
-    new Card("Quetzalmon", 470),
-    new Card("Phoenixmon", 530),
-    new Card("Exveemon", 460),
-    new Card("Togemon", 360),
-    new Card("Leomon", 400),
-    new Card("Birdramon", 410),
-    new Card("Garudamon", 460),
-    new Card("Piximon", 470),
-    new Card("Liamon", 560),
+    new Card("Saberleomon", 550, 310, 2000, 3, 2),
+    new Card("Wargreymon", 540, 350, 3000, 5, 1),
+    new Card("Magnadramon", 520, 350, 3000, 1, 1),
+    new Card("Devimon", 350, 260, 1000, 7, 3),
+    new Card("Lilymon", 510, 380, 2000, 2, 2),
+    new Card("Angemon", 560, 200, 1000, 2, 3),
+    new Card("Angewomon", 470, 340, 2000, 2, 2),
+    new Card("Rosemon", 570, 380, 3000, 4, 1),
+    new Card("Seraphimon", 540, 340, 3000, 1, 1),
+    new Card("Ofanimon", 540, 420, 3000, 2, 1),
+    new Card("Gatomon", 360, 210, 1000, 4, 3),
+    new Card("Nefertimon", 470, 270, 1000, 1, 3),
+    new Card("Pagasusmon", 420, 230, 1000, 1, 3),
+    new Card("Kyubimon", 450, 210, 1000, 4, 3),
+    new Card("Sakuyamon", 560, 570, 3000, 3, 1),
+    new Card("Myotismon", 510, 280, 2000, 10, 2),
+    new Card("Piedmon", 530, 340, 3000, 9, 1),
+    new Card("Puppetmon", 500, 340, 3000, 8, 1),
+    new Card("Ladydevimon", 520, 300, 2000, 10, 2),
+    new Card("Weregarurumon", 530, 240, 2000, 5, 2),
+    new Card("Lynxmon", 480, 240, 2000, 5, 3),
+    new Card("Quetzalmon", 470, 230, 2000, 6, 2),
+    new Card("Phoenixmon", 530, 350, 3000, 4, 1),
+    new Card("Exveemon", 460, 230, 1000, 5, 3),
+    new Card("Togemon", 360, 200, 1000, 2, 3),
+    new Card("Leomon", 400, 200, 1000, 1, 3),
+    new Card("Birdramon", 410, 200, 1000, 5, 3),
+    new Card("Garudamon", 460, 260, 2000, 4, 2),
+    new Card("Piximon", 470, 220, 2000, 5, 2),
+    new Card("Liamon", 560, 300, 3000, 5, 1),
 ]
 
 // const deck2 = [
@@ -75,8 +93,8 @@ const randomDeck1Loop = ()=>{
 randomDeck1Loop()
 
 const drawCardPlayer1 = ()=>{
-    let player1 = randomCardArr.splice(0,15);
-    console.log(player1);
+    player1 = randomCardArr.splice(0,15);
+    // console.log(player1);
 
     console.log(`this is player 1 deck ${player1[0].name}`);
     console.log(`this is player 1 deck ${player1[0].attack}`);
@@ -85,8 +103,8 @@ const drawCardPlayer1 = ()=>{
 drawCardPlayer1()
 
 const drawCardPlayer2 = () =>{
-    let player2 = randomCardArr.splice(0,15);
-    console.log(player2);
+    player2 = randomCardArr.splice(0,15);
+    // console.log(player2);
 
     console.log(`this is player 2 deck ${player2[0].name}`);
     console.log(`this is player 2 deck ${player2[0].attack}`)
@@ -94,11 +112,11 @@ const drawCardPlayer2 = () =>{
 drawCardPlayer2()
 
 const winOrLose = () =>{
-    if(drawCardPlayer1.attack < drawCardPlayer2.attack){
-        console.log('player2 wins')
+    if(player1[0].attack > player2[0].attack){
+        console.log('player 1 wins')
     }
     else{
-        console.log('player1 wins')
+        console.log('player 2 wins')
     }
 }
 winOrLose()
